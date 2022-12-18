34058f10d94392c4d1a2e118d9add0ae1aa20aeb
Tunahan Ugrasir <tunahannugrasir@gmail.com>
2022-12-18 22:28:52 +0300

    usefull packages and uses

---
+++ lib/product/init/language/locale_keys.g.dart
@@ -0,0 +1,8 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const app_title = 'app.title';
+  static const app_description = 'app.description';
+  static const app = 'app';
+
+}
